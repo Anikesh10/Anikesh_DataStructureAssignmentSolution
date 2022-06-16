04bcd011300772070380283c7751201ba7a745a0
anikesh10 <anikeshrajan@gmail.com>
2022-06-16 19:48:47 +0530

    Data structure graded assignment

---
+++ src/toSkewedTree/SkewedTree.java
@@ -0,0 +1,16 @@
+package toSkewedTree;
+
+public class SkewedTree {
+
+	public void printSkewedTree(Node aNode) {
+
+		if (aNode == null) {
+			return;
+		}
+
+		printSkewedTree(aNode.getLeftNode());
+		System.out.println(aNode.getData());
+		printSkewedTree(aNode.getRightNode());
+	}
+
+}
